66b6ed2de60cdf05d982c42c3c229c646bc727fb
Pentacles-A <adewaleabdulgafar@gmail.com>
2022-03-13 17:40:53 +0000

    ff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Use printf to print out the different type sizes
  *
- * Return: Always 0 (Success)
+ * Return: A printed string stating the different type sizes
  */
 int main(void)
 {
 	char c;
 	int d;
-	long int e;
-	long long int f;
+	long int ei;
+	long long int ffi;
 	float g;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("SIze of a long int: %li byte(s)\n", (long unsigned int)sizeof(e));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("SIze of a long int: %lu byte(s)\n", (long unsigned)sizeof(ei));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ffi));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
 
 	return (0);
