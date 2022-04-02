7f1724bc41c9f6ca8378e5550664efe94bf00e20
Pentacles-A <adewaleabdulgafar@gmail.com>
2022-04-02 13:00:35 +0000

    The function libraries

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[] __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
